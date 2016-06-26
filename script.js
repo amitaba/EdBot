496fd6324ae1a46ee79f13f26a650fb5ee2868c4
amitaba <amitaba@parasam.com>
2016-06-26 12:58:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('So you want to learn about Ed? Just say HELLO to get started.')
+            return bot.say('So do you want to learn about Ed? Just say HELLO to get started.')
                 .then(() => 'speak');
         }
     },
